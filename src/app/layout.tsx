f43dc1cc414ce8b9c32f9e126803875be8278c35
mohsen mohebbi <mohsen56600733@gmail.com>
2025-08-20 14:59:51 +0330

    Add Persian font and RTL styling for the

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import "../styles/globals.scss";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fa" dir="rtl">
-      <body>{children}</body>
+    <html lang="fa">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v30.1.0/dist/font-face.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </head>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
